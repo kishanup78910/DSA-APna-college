8c78e131d1c6ea2cb04e34a9602bbe298809f3ae
kishanengineosol <kishanu@engineosol.com>
2024-12-08 11:52:54 +0530

    Part1 Completed

---
+++ 2VariableANDdataSTructure/Varible.java
@@ -0,0 +1,8 @@
+public class Varible {
+    public static void main(String[] args) {
+        
+        String a="kishan";
+        int b=24;
+        System.out.println(a + " is " + b + " years old");
+    }
+}
